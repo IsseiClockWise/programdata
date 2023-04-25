76e418f58d76c647edb8546d8de3764c9f1fdc2d
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-04-26 08:43:22 +0900

    追加

--- loop.py
+++ loop.py
@@ -1,5 +1,14 @@
-for i in range(1, 6):
-    if i <= 3:
-        print("o" * i)
-    else:
-        print("o" * (6-i))
+# for i in range(1, 6):
+#     if i <= 3:
+#         print("o" * i)
+#     else:
+#         print("o" * (6-i))
+
+lst = ["o", "o", "o"]
+
+for i in range(1, 5):
+    print("".join(lst[:i]))
+
+for i in range(3, 0, -1):
+    print("".join(lst[:i]))
+
