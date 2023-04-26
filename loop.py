30f6f8e5ef3a2b60d335d28ef427299e33707ed1
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-04-26 10:07:17 +0900

    tt

--- loop.py
+++ loop.py
@@ -1,7 +1,7 @@
-lst = ["o", "o", "o"]
+list = ["o", "o", "o"]
 
 for i in range(1, 3):
-    print("".join(lst[:i]))
+    print("".join(list[:i]))
 
 for i in range(3, 0, -1):
-    print("".join(lst[:i]))
+    print("".join(list[:i]))
