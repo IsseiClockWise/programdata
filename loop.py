f9f3bed8981ae106f90316c0e8cd527f14bc3a96
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-04-26 10:06:30 +0900

    修正

--- loop.py
+++ loop.py
@@ -1,14 +1,7 @@
-# for i in range(1, 6):
-#     if i <= 3:
-#         print("o" * i)
-#     else:
-#         print("o" * (6-i))
-
 lst = ["o", "o", "o"]
 
-for i in range(1, 5):
+for i in range(1, 3):
     print("".join(lst[:i]))
 
 for i in range(3, 0, -1):
     print("".join(lst[:i]))
-
